de4b759ba493a79fb0cbe40ae391dc1825705002
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-05-10 16:19:18 -0700

    initial version + readme

---
+++ user-search/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const people = [
+  "1User",
+  "12User",
+  "14User",
+  "13User",
+  "15User",
+  "16User",
+  "17User"
+];
+function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  React.useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-search/src/components/inputArea.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+
+const people = [
+    "1User",
+    "12User",
+    "13User",
+    "14User",
+    "16User",
+    "15User",
+    "17User"
+  ];
+
+const inputArea= ()=>(
+const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  React.useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+
+
+
+
+)
+
+
+export default inputArea;
+
+
